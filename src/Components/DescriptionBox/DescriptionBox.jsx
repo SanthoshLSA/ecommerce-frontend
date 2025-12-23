5c07dde35c648391111adc1206ffb27440705f26
SanthoshLSA <santhosh.ananth6@gmail.com>
2025-12-23 23:42:01 +0530

    frontend-non-responsive

---
+++ src/Components/DescriptionBox/DescriptionBox.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionbox'>
+      <div className="descriptionbox-navigator">
+        <div className="descriptionbox-nav-box">Description</div>
+        <div className="descriptionbox-nav-box fade">Reviews (122)</div>
+      </div>
+      <div className="descriptionbox-description">
+        E-commerce website is an online platform
+      </div>
+    </div>
+  )
+}
+
+export default DescriptionBox
